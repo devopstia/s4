eb6424919af2e123302fc2192db0aa722f542155
DevOps Easy Learning <support@devopseasylearning.com>
2023-06-11 14:52:23 -0500

    terraform

--- terroform/session04/resources/eks-control-plane/dev/main.tf
+++ terroform/session04/resources/eks-control-plane/dev/main.tf
@@ -24,7 +24,7 @@ terraform {
 
 locals {
   aws_region              = "us-east-1"
-  control_plane_version   = "1.22"
+  control_plane_version   = "1.23"
   endpoint_private_access = false
   endpoint_public_access  = true
 
